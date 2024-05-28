aae9cf0a61ed1aa2d6e96661963a7e86f89de9e9
Joe <joeegboka@gmail.com>
2024-05-28 19:31:45 +0100

    2-app.py

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """ 2 app module """
 
-from flask import Flask, request
+from flask import Flask, request, render_template
 from flask_babel import Babel
 
 app = Flask(__name__)
@@ -23,6 +23,12 @@ app.config.from_object(Config)
 def get_locale() -> str:
     """get locale"""
     return request.accept_languages.best_match(app.config["LANGUAGES"])
+
+
+@app.route("/", strict_slashes=False)
+def home():
+    """Home route"""
+    return render_template("2-index.html")
 
 
 if __name__ == "__main__":
