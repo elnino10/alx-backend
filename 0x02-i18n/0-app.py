b7fea6d677cf04a437d4342423308ff9d374882b
Joe <joeegboka@gmail.com>
2024-05-28 15:44:23 +0100

    1-app.py

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+""" flask app module """
+
+from flask import Flask
+
+app = Flask(__name__)
---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+""" flask app module """
+
+from flask import Flask
+from flask_babel import Babel
+
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config:
+    """Config class"""
+
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
