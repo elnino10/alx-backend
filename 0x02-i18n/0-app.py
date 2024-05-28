2dddca8a4b99978dd117c4e5b178b3191f0af606
Joe <joeegboka@gmail.com>
2024-05-28 15:55:20 +0100

    1-app.py

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,6 +1,16 @@
 #!/usr/bin/env python3
 """ flask app module """
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def home():
+    """Home route"""
+    return render_template("0-index.html")
+
+
+if __name__ == "__main__":
+    app.run()
--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """ flask app module """
 
-from flask import Flask
+from flask import Flask, render_template
 from flask_babel import Babel
 
 
@@ -18,3 +18,13 @@ class Config:
 
 
 app.config.from_object(Config)
+
+
+@app.route("/", strict_slashes=False)
+def home():
+    """Home route"""
+    return render_template("1-index.html")
+
+
+if __name__ == "__main__":
+    app.run()
