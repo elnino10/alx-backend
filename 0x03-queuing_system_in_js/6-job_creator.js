0e2699996ce0116d1b751fe81921147f75c768d8
Joe Egboka <joeegboka@gmail.com>
2024-06-26 20:03:36 +0100

    6-job_creator.js

---
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -0,0 +1,22 @@
+import { createQueue } from "kue";
+
+const queue = createQueue();
+
+const jobDataFormat = {
+    phoneNumber: '08036943113',
+    message: 'The first phone number',
+}
+
+const job = queue.create('push_notification_code', jobDataFormat)
+job
+.on('enqueue', () => {
+    console.log(`Notification job created: ${job.id}`);
+})
+.on('failed attempt', () => {
+    if (error) console.log('Notification job failed');
+})
+.on('completed', () => {
+    console.log('Notification job completed');
+})
+
+job.save();
